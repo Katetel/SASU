ff444ee15957aebaab25c3a3bd666889a653fb20
tnqudqor117 <tnqudqor1@naver.com>
2018-06-26 18:21:35 +0900

    [add] 로그인 / 회원가입 / 핸드폰인증

---
+++ app/src/main/java/com/eternal/sasu/JoinPhone.java
@@ -0,0 +1,13 @@
+package com.eternal.sasu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JoinPhone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_phone);
+    }
+}
